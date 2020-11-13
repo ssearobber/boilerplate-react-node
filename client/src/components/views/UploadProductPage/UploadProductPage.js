ea6bd1750e891f7699d4b328adcaee6e59df7d72
ssearobber <ssearobber@naver.com>
2020-11-13 17:35:09 +0900

    first commit

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UploadProductPage = () => {
+  return (
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <h2>테스트</h2>
+      </div>
+    </div>
+  );
+};
+
+export default UploadProductPage;
